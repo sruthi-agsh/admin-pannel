c2fe307a0d496af9f51b18b76f72c4b7ea0401c5
sruthi <shruthiageesh@gmail.com>
2026-05-11 21:00:23 +0530

    changes upadated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
+// Add CORS
+server.use(cors({
+  origin: "https://admin-pannel-sigma-three.vercel.app",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+}));
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
@@ -9,5 +16,5 @@ server.use(router);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server running on port ${PORT}`);
 });
